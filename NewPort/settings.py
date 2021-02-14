9f3c94a492415cdfa4ce704b63e79b12a6e8a881
Ashish Mishra <i.m.ashishhh@gmail.com>
2021-02-14 13:07:02 +0530

    Update settings.py

--- NewPort/settings.py
+++ NewPort/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'NewPort.urls'
@@ -127,5 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
